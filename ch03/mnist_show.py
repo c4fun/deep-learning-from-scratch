ace7f18f5e7659f74d3a8332f1129502fe912573
c4fun <pcfeng502@gmail.com>
2024-01-04 22:02:50 +0800

    mnist cn

--- ch03/mnist_show.py
+++ ch03/mnist_show.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
@@ -10,12 +10,14 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
+# 读入数据（第一次需要花费几分钟，下载数据集）
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
 img = x_train[0]
 label = t_train[0]
 print(label)  # 5
 
+# 输出各个数据的形状
 print(img.shape)  # (784,)
 img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
 print(img.shape)  # (28, 28)
