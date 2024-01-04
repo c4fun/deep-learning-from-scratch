de44fda1fcd29d8bd194851a740abafc9fd2ce4e
c4fun <pcfeng502@gmail.com>
2024-01-04 22:22:19 +0800

    add mnist batch cn

---
+++ ch03/neuralnet_mnist_batch_cn.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir) # 为了导入父目录中的文件而进行的设定
+import numpy as np
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = \
+        load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3'] # 读入权重和偏置
+    b1, b2, b3 = network['b1'], network['b2'], network['b3'] # 读入权重和偏置
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3) # 输出层的激活函数
+    return y
+
+
+x, t = get_data() # 读入数据
+network = init_network() # 读入权重和偏置
+
+batch_size = 200
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+	x_batch = x[i:i+batch_size]
+	y_batch = predict(network, x_batch)
+	p = np.argmax(y_batch, axis=1)
+	accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x))) # 输出正确率
